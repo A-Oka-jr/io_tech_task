a3571fae154858f385bf4aa3d043c7cb576487ee
Ahmed Okasha <aokasha1@outlook.com>
2025-05-16 16:10:24 +0400

    first

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full flex justify-center items-center h-16 z-10 bg-black bg-opacity-50">
+      <h1 className="text-white text-2xl font-bold">My Navbar</h1>
+    </nav>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div className="h-screen w-full bg-cover bg-center flex items-center justify-center"
+  style={{
+    backgroundImage: "url('https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
+  }}>
+      <h1 className="text-3xl text-blue-800">This is the Index Page</h1>
+    </div>
+  )
+}
+export default Home
