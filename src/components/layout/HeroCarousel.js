5fc9a731be215cf25162deacfc1d58c09dd0e795
Ahmed Okasha <aokasha1@outlook.com>
2025-05-17 10:30:17 +0400

    compliting our team and Testimonial section

--- src/components/layout/HeroCarousel.js
+++ src/components/layout/HeroCarousel.js
@@ -1,23 +1,24 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/effect-fade';
-import { EffectFade, Autoplay } from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function HeroCarousel() {
   return (
     <Swiper
-      modules={[EffectFade, Autoplay]}
-      effect="fade"
+      modules={[Autoplay, Navigation, Pagination]}
       autoplay={{ delay: 3000, disableOnInteraction: false }}
       loop
+      navigation
+      pagination={{ clickable: true, type: 'fraction' }}
       className="h-screen w-full"
     >
       <SwiperSlide>
         <div
           className="h-screen w-full bg-cover bg-center"
           style={{
-            backgroundImage:
-              "url('https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
+            backgroundImage: "url('/slider1.jpg')",
           }}
         ></div>
       </SwiperSlide>
@@ -25,8 +26,7 @@ export default function HeroCarousel() {
         <div
           className="h-screen w-full bg-cover bg-center"
           style={{
-            backgroundImage:
-              "url('https://source.unsplash.com/random/1600x900')",
+            backgroundImage: "url('/slider1.jpg')",
           }}
         ></div>
       </SwiperSlide>
@@ -34,8 +34,7 @@ export default function HeroCarousel() {
         <div
           className="h-screen w-full bg-cover bg-center"
           style={{
-            backgroundImage:
-              "url('https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
+            backgroundImage: "url('/slider1.jpg')",
           }}
         ></div>
       </SwiperSlide>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-import HeroCarousel from '../components/layout/HeroCarousel';
 import { useTranslation } from 'react-i18next'
 
 export default function Home() {
     const { t } = useTranslation()
   return (
     <div className="h-screen w-full relative">
-      <HeroCarousel />
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-white text-4xl font-bold">{t('welcome')}</h1>
+        <h1 className="text-black text-4xl font-bold">{t('welcome')}</h1>
       </div>
     </div>
   );
